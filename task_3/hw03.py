d11bf08d42010a68685d51e25ad24b398a816d2f
didukhroma <didukhroma@gmail.com>
2025-02-18 21:59:03 +0200

    hw-03 added requirements file

--- task_3/hw03.py
+++ task_3/hw03.py
@@ -8,7 +8,7 @@ def main():
         path = sys.argv[1]
         directory = Path(path)
         if not directory.exists():
-            raise Exception("Directory does not exist. Please check the path.")
+            raise Exception("Directory does not exist. Please check the absolute path.")
         
         log_folder_name(directory.name,space_count=0)
         show_directory(path,space_count=1)
